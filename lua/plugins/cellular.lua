49181eac5cf2df5ba462fb5d0ec3da8cc9fabc4e
BeastWalnut <Dieguitoleyva12+git@gmail.com>
2024-02-04 20:17:52 -0700

    plugin: miscelaneous plugins

---
+++ lua/plugins/cellular.lua
@@ -0,0 +1,8 @@
+return {
+	"eandrju/cellular-automaton.nvim",
+	keys = {
+		{ "<leader>ar", "<cmd>CellularAutomaton make_it_rain<CR>", desc = "[A]utomaton [R]ain" },
+		{ "<leader>al", "<cmd>CellularAutomaton game_of_life<CR>", desc = "[A]utomaton [L]ife" },
+	},
+}
+
---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,38 @@
+return {
+    "folke/which-key.nvim",
+    lazy = true,
+    init = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 500
+    end,
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+    },
+    config = function(_, opts)
+        local wk = require("which-key")
+        wk.setup(opts)
+
+        local keymap = {
+            e = { name = "File Explorer" },
+            u = { name = "UndoTree" },
+            h = { name = "Harpoon" },
+            f = { name = "Find" },
+            c = { name = "Lsp" },
+        }
+
+        local key_opts = {
+            "n",
+            prefix = "<leader>",
+        }
+
+        wk.register(keymap, key_opts)
+
+        local presets = require("core.keymaps")
+
+        for _, preset in ipairs(presets) do
+            wk.register(preset.maps, preset.opts)
+        end
+    end,
+}

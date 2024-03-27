9b2ff526a93c881ab5901559f43669647af8d96d
BeastWalnut <Dieguitoleyva12+git@gmail.com>
2024-03-27 11:47:26 -0700

    changed description of keymaps

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,38 +1,40 @@
 return {
-    "folke/which-key.nvim",
-    lazy = true,
-    init = function()
-        vim.o.timeout = true
-        vim.o.timeoutlen = 500
-    end,
-    opts = {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-    },
-    config = function(_, opts)
-        local wk = require("which-key")
-        wk.setup(opts)
+	"folke/which-key.nvim",
+	lazy = true,
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 500
+	end,
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	},
+	config = function(_, opts)
+		local wk = require("which-key")
+		wk.setup(opts)
 
-        local keymap = {
-            e = { name = "File Explorer" },
-            u = { name = "UndoTree" },
-            h = { name = "Harpoon" },
-            f = { name = "Find" },
-            c = { name = "Lsp" },
-        }
+		local keymap = {
+			a = { name = "Automata" },
+			t = { name = "Trouble" },
+			e = { name = "File Explorer" },
+			u = { name = "UndoTree" },
+			h = { name = "Harpoon" },
+			f = { name = "Find" },
+			c = { name = "Lsp" },
+		}
 
-        local key_opts = {
-            "n",
-            prefix = "<leader>",
-        }
+		local key_opts = {
+			"n",
+			prefix = "<leader>",
+		}
 
-        wk.register(keymap, key_opts)
+		wk.register(keymap, key_opts)
 
-        local presets = require("core.keymaps")
+		local presets = require("core.keymaps")
 
-        for _, preset in ipairs(presets) do
-            wk.register(preset.maps, preset.opts)
-        end
-    end,
+		for _, preset in ipairs(presets) do
+			wk.register(preset.maps, preset.opts)
+		end
+	end,
 }

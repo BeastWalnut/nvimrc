069cf805dfc6c8fa6253ff27e0bb3c85dd0b62bb
BeastWalnut <Dieguitoleyva12@gmail.com>
2024-07-08 12:05:32 -0700

    chore(lspconfig): keymap documentation

--- lua/lsp/lspconfig.lua
+++ lua/lsp/lspconfig.lua
@@ -4,26 +4,35 @@ return {
     config = function()
         local augroup = vim.api.nvim_create_augroup
         local autocmd = vim.api.nvim_create_autocmd
+        local lsp_format = augroup("LspFormat", {})
         vim.api.nvim_create_autocmd("LspAttach", {
             group = augroup("LspOnAttach", {}),
             callback = function(e)
                 local map_opts = {
+                    name = "LSP",
                     noremap = true,
                     buffer = e.buf,
                 }
 
                 Set_map({
-                    ["<M-q>"] = { vim.lsp.buf.format, "Code Format" },
-                    K = { vim.lsp.buf.hover, "Cursor Doc" },
+                    ["<M-q>"] = { vim.lsp.buf.format, "Format" },
+                    K = { vim.lsp.buf.hover, "Open Docs" },
                 }, map_opts)
 
                 map_opts.mode = "i"
                 Set_map({
-                    ["<C-h>"] = { vim.lsp.buf.signature_help, "Open lsp help" },
+                    ["<C-h>"] = { vim.lsp.buf.signature_help, "Open Help" },
+                }, map_opts)
+
+                map_opts.mode = { "n", "v" }
+                map_opts.prefix = "<leader>"
+                Set_map({
+                    rn = { vim.lsp.buf.rename, "[R]ename" },
                 }, map_opts)
 
                 map_opts.mode = "n"
                 map_opts.name = "Diagnostic"
+                map_opts.prefix = nil
                 Set_map({
                     ["]d"] = { vim.diagnostic.goto_next, "Next" },
                     ["[d"] = { vim.diagnostic.goto_prev, "Previous" },
@@ -36,22 +45,18 @@ return {
                     r = { vim.lsp.buf.references, "Peek [R]eferences" },
                 }, map_opts)
 
-                if vim.g.loaded_telescope then
-                    Set_map({
-                        d = { "<cmd>Telescope lsp_definitions<CR>", "Peek [D]eclaration" },
-                        i = { "<cmd>Telescope lsp_implementations<CR>", "Peek [I]mplementation" },
-                    }, map_opts)
-                end
+                Set_map({
+                    d = { "<cmd>Telescope lsp_definitions<CR>", "Peek [D]eclarations" },
+                    i = { "<cmd>Telescope lsp_implementations<CR>", "Peek [I]mplementations" },
+                }, map_opts)
 
                 map_opts.mode = { "n", "v" }
                 map_opts.prefix = "<leader>"
                 Set_map({
                     ca = { vim.lsp.buf.code_action, "[C]ode [A]ctions" },
-                    rn = { vim.lsp.buf.rename, "Cursor [R]ename" },
-                    e = { vim.diagnostic.open_float, "Cursor Diagnostic" },
+                    e = { vim.diagnostic.open_float, "Open Float" },
                 }, map_opts)
 
-                local lsp_format = augroup("LspFormat", {})
                 vim.api.nvim_clear_autocmds({ group = lsp_format, buffer = e.buf })
                 autocmd("BufWritePre", {
                     group = lsp_format,
